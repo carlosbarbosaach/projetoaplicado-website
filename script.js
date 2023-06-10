f536020497665ac501fc8d58eb27c2cec3929619
Mateus Henrique <mateushrb@hotmail.com>
2023-06-10 20:09:01 -0300

    adjust bikemania

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = '0';
 }
+
+function bikeSpeed() {
+    let maxSpeed = parseInt(document.getElementById('bike-speed').innerHTML);
+    const speedIncrease = document.getElementById('bike-speed');
+    speedIncrease.innerHTML = 0
+    let increase = 0
+    setInterval(() => {
+        if (speedIncrease.innerHTML < maxSpeed) {
+            speedIncrease.innerHTML = increase
+            increase++
+        }
+    }, 30)
+}
+bikeSpeed();
